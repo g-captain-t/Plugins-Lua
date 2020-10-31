e73788d6d9289cf026f86b14a1cbd5182131571f
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-10-31 11:11:47 +0700

    Thumbnail, titles, variable fix
    Plugin is published at https://www.roblox.com/library/5899442773/

--- WeldConstraintPlugin/Main.lua
+++ WeldConstraintPlugin/Main.lua
@@ -2,29 +2,29 @@ local Selection = game:GetService("Selection")
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
 
 local function Weld(part, part1)
-  local wc = Instance.new("WeldConstraint")
-  wc.Part0 = part1 wc.Part1 = part
-  wc.Parent = part1
+	local wc = Instance.new("WeldConstraint")
+	wc.Part0 = part1 wc.Part1 = part
+	wc.Parent = part1
 end
 
 local function Main()
-  local currentSelections = Selection:Get()
-  if currentSelections[1] == nil then warn("Can't weld: Select a group or several parts first to weld!") return end
+	local currentSelections = Selection:Get()
+	if currentSelections[1] == nil then warn("Can't weld: Select a group or several parts first to weld!") return end
 
-  local parts = {}
-  for i, selection in ipairs (currentSelections) do 
-    if selection:IsA("BasePart") then table.insert(parts,selection) 
-      else for i, v in pairs(selection:GetDescendants()) do if v:IsA("BasePart") then table.insert(parts,v) end end
-    end 
-  end
-  for i, v in ipairs(parts) do if i~=1 then Weld(v,parts[1]) end end
+	local parts = {}
+	for i, selection in ipairs (currentSelections) do 
+		if selection:IsA("BasePart") then table.insert(parts,selection) 
+		else for i, v in pairs(selection:GetDescendants()) do if v:IsA("BasePart") then table.insert(parts,v) end end
+		end 
+	end
+	for i, v in ipairs(parts) do if i~=1 then Weld(v,parts[1]) end end
 
-  ChangeHistoryService:SetWaypoint("Quick WeldConstraints")
-  warn("Parts successfully welded to "..part[1].Name.."!")
+	ChangeHistoryService:SetWaypoint("Quick WeldConstraints")
+	warn("Parts successfully welded to "..parts[1].Name.."!")
 end
 
 -- Bind to button
-local openPlugin = plugin:CreateToolbar("CameraLight"):CreateButton("Quick WeldConstraint","Select a group or several parts to weld!","rbxassetid://0")
+local openPlugin = plugin:CreateToolbar("Quick WeldConstraints"):CreateButton("Quick Weld","Select a group or several parts to weld to the first selected part!","rbxassetid://5899414710")
 openPlugin.Click:Connect(Main)
 
 -- Plugin version of WeldConstraint welding by g_captain
