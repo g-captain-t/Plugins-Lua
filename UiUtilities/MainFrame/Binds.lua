694abdcef674a05cae4a35501c67e2385f37e042
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-11-20 18:32:22 +0700

    Major updates
    - Offset sizing & positioning
    - Size & Positions now support SizeConstraints relativity
    - Clone & Swap Properties
    - Font Size

--- UiUtilities/MainFrame/Binds.lua
+++ UiUtilities/MainFrame/Binds.lua
@@ -2,7 +2,6 @@ local Tools = require(script.Parent.MainModule)
 local UI = require(script.Parent.UIModule)
 
 --- General 
-
 local AnchorPoints = script.Parent.Gen_Anchor
 for i,v in pairs (AnchorPoints:GetChildren()) do
 	v.MouseButton1Click:Connect(function()Tools.Anchor(v.AnchorPoint, v.Position) end)
@@ -11,6 +10,16 @@ end
 local Fonts = script.Parent.Gen_Font
 Fonts.All.MouseButton1Click:Connect(function()Tools.Font(Fonts.Input.Text, true)end)
 Fonts.Selected.MouseButton1Click:Connect(function()Tools.Font(Fonts.Input.Text, false) end)
+
+local FontSize = script.Parent.Gen_FontSize
+FontSize.All.MouseButton1Click:Connect(function()Tools.FontSize(FontSize.Input.Text, true)end)
+FontSize.Selected.MouseButton1Click:Connect(function()Tools.FontSize(FontSize.Input.Text, false) end)
+
+local CopyProps = script.Parent.Gen_PropertyCopy
+CopyProps.Button.MouseButton1Click:Connect(function() Tools.CopyProperty(CopyProps.Input.Text) end)
+
+local SwapProps = script.Parent.Gen_PropertySwap
+SwapProps.Button.MouseButton1Click:Connect(function() Tools.SwapProperty(SwapProps.Input.Text) end)
 
 --- Frame Scale Actions
 
@@ -30,3 +39,10 @@ local SO_Size = ScaleObj.size
 UI.ConnectToggle(SO_Pos); UI.ConnectToggle(SO_Size)
 ScaleObj.ScaleAll.MouseButton1Click:Connect(function()Tools.Scale(SO_Size.RichText, SO_Pos.RichText, true) end)
 ScaleObj.ScaleSelect.MouseButton1Click:Connect(function()Tools.Scale(SO_Size.RichText, SO_Pos.RichText, false) end)
+
+local OffsetObj = script.Parent.Scale_OffsetObj
+local OO_Pos = OffsetObj.position
+local OO_Size = OffsetObj.size
+UI.ConnectToggle(OO_Pos); UI.ConnectToggle(OO_Size)
+OffsetObj.OffsetAll.MouseButton1Click:Connect(function()Tools.Offset(OO_Size.RichText, OO_Pos.RichText, true) end)
+OffsetObj.OffsetSelect.MouseButton1Click:Connect(function()Tools.Offset(OO_Size.RichText, OO_Pos.RichText, false) end)
