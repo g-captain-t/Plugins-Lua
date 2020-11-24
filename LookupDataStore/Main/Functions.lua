674ec1cb8117cd5fe8ccedc161cf655b125180af
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-11-24 23:12:12 +0700

    OrderedDataStore, Scope

--- LookupDataStore/Main/Functions.lua
+++ LookupDataStore/Main/Functions.lua
@@ -2,36 +2,35 @@ local module = {}
 local DS = game:GetService("DataStoreService")
 local HTTPS = game:GetService("HttpService")
 local History = game:GetService("ChangeHistoryService")
-local DSS = require(script.DSS) print(DSS)
+local DSS = require(script.DSS)
 
-function module.Lookup(Name, Key, OutputBox)
-	local store = DSS.GetDataStore(Name) print(store)
+function module.Lookup(Name, Key, OutputBox, isOrdered, scope)
+	local store = isOrdered and DSS.GetOrderedStore(Name, scope) or DSS.GetDataStore(Name, scope) print(store)
 	local success, entry = DSS.Get(store,Key) print(entry)
 	local jsonString = HTTPS:JSONEncode(entry) print(jsonString)
 	OutputBox.Text = jsonString
 end
 
-function module.Save(Name, Key, newJSON)
+function module.Save(Name, Key, newJSON, isOrdered, scope)
 	History:SetWaypoint("Changing "..Name.." "..tostring(Key).." to "..newJSON)
-	local store = DSS.GetDataStore(Name)	print(store)
+	local store = isOrdered and DSS.GetOrderedStore(Name) or DSS.GetDataStore(Name)	print(store)
 	local newEntry = HTTPS:JSONDecode(newJSON)	print(newJSON,newEntry)
 	local success, err = DSS.Set(store, Key, newEntry) print(success, err)
+	print("Changed "..Name.." "..tostring(Key).." to "..newJSON)
 	History:SetWaypoint("Changed "..Name.." "..tostring(Key).." to "..newJSON)
 end
 
 return module
 
 
---[[
+--[[ NOTE TO SELF
 To test, select the DSS module and paste this in command bar:
 local DS = game:GetService("DataStoreService") local DSS = require(game.Selection:Get()[1])
 local Store = DS:GetDataStore("DSPluginTest")
 DSS.Set(Store,"Entry1",
 {
 	["Foo"] = 10; 
-	["Bar"] = 13;}
+	["Bar"] = false;}
 )
-
-
 Lookup DSPluginTest, Entry1
 ]]
