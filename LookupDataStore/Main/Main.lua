120ac33296916c3387e7af7ea2682ca5ca190d00
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-11-24 23:12:44 +0700

    OrderedDataStore, Scope, Renaming

--- LookupDataStore/Main/Main.lua
+++ LookupDataStore/Main/Main.lua
@@ -1,14 +1,19 @@
 local Sidebar = script.Parent.Sidebar
 local Content = script.Parent.Scroll.Content
 local Main = require(script.Parent.Functions)
+local UI = require(script.Parent.UIModule)
 
+local isOrdered = Sidebar.Toggles_IsOrdered
+UI.ConnectToggle(isOrdered, "RichText")
 
-local KeyInput = Sidebar.KeyInput
-local NameInput = Sidebar.NameInput
-Sidebar.Lookup.MouseButton1Click:Connect(function()
-	Main.Lookup(NameInput.Text, KeyInput.Text, Content)
+local KeyInput = Sidebar.Input_Key
+local NameInput = Sidebar.Input_Name
+local ScopeInput = Sidebar.Input_Scope
+
+Sidebar.OP_Lookup.MouseButton1Click:Connect(function()
+	Main.Lookup(NameInput.Text, KeyInput.Text, Content, isOrdered.RichText, ScopeInput.Text)
 end)
 
-Sidebar.Save.MouseButton1Click:Connect(function()
-	Main.Save(NameInput.Text, KeyInput.Text, Content.Text)
+Sidebar.OP_Save.MouseButton1Click:Connect(function()
+	Main.Save(NameInput.Text, KeyInput.Text, Content.Text, isOrdered.RichText, ScopeInput.Text)
 end)
